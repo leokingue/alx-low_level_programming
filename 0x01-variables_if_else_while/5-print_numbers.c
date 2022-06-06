61f1ddce3175c9ef9056ddca8a74eb0e85a651f2
leokingue <ebangkingue@icloud.com>
2022-06-06 16:42:39 -0700

    ed

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-printing single digit from 0 to 10
+ *
+ *Return:0 if success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
+
