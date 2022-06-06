fa9491e9d05a4a6279cfea41d688a26a35f4af2c
leokingue <ebangkingue@icloud.com>
2022-06-06 16:06:00 -0700

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
 /**
  *main-printing alphabet in lowwercase
  *
@@ -6,14 +8,24 @@
  */
 int main(void)
 {
-	char chara;
+	int n;
 
-	for (chara = 'a'; chara <= 'z'; chara++)
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if ((n % 10) > 5)
 	{
-		putchar(chara);
+		printf("Last digit of %d is %d and is greater than 5", n, (n % 10));
 	}
-	putchar('\n');
-
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, (n % 10));
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0", n, (n % 10));
+	}
+	printf("\n");
 	return (0);
 
 }
