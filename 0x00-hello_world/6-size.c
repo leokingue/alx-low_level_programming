c1ae6ad67025b53ff00441f3eadd311d8ad2eba3
leokingue <ebangkingue@icloud.com>
2022-06-06 14:36:59 -0700

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 /**
  *main - print where we begin
- *
- *
  * Return:  return zero to declare sucess
  */
 int main(void)
@@ -13,11 +11,8 @@ int main(void)
 	long long int lli;
 	float f;
 
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(long long int));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(float));
+	printf("Size of a char:%lubyte(s)\n", (unsigned long) sizeof(char));
+	printf("Size of an int:%lubyte(s)\n", (unsigned long) sizeof(int));
+	printf("Size of a long int:%lubyte(s)\n", (unsigned long) sizeof(long int));	printf("Size of a float:%lubyte(s)\n", (unsigned long) sizeof(float));
 	return (0);
 }
