5ac6cc699dc3e814291156b097ad4a92cf389180
leokingue <ebangkingue@icloud.com>
2022-06-06 16:11:26 -0700

    lsg

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ *main-printing alphabet in lowwercase
+ *
+ *Return:0 if success
+ */
+int main(void)
+{
+	char chara;
+
+	for (chara = 'a'; chara <= 'z'; chara++)
+	{
+		putchar(chara);
+	}
+	putchar('\n');
+
+	return (0);
+}
