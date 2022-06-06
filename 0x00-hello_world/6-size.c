e4a61981576e1fd68620937e6a4a699f7f7c3fa9
leokingue <ebangkingue@icloud.com>
2022-06-06 14:15:31 -0700

    update

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ *main - print where we begin
+ *
+ *
+ * Return:  return zero to declare sucess
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(float));
+	return (0);
+}
