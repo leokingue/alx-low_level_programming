bcb35f4077b6724b247a706422fbc06c98692624
leokingue <ebangkingue@icloud.com>
2022-06-06 16:26:27 -0700

    der

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main-printing alphabet in lowwercase and skip q, e
+ *
+ * Return:0 if success
+ */
+int main(void)
+{
+	char chara;
+
+	for (chara = 'a'; chara <= 'z'; chara++)
+	{
+		if (chara == 'e' || chara == 'q')
+		{
+			continue;
+		}
+		putchar(chara);
+	
+	}
+	putchar('\n');
+	return (0);
+
+}
