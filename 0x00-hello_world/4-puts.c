5d41da7b3ea429b1a785f9e26276d4ac4dbf029e
leokingue <ebangkingue@icloud.com>
2022-06-06 13:05:33 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print where we begin
+ * Return:  return zero to declare sucess
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
