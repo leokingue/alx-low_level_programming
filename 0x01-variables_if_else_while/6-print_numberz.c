4844f187448b47850a68acab8248d3e9211e6585
leokingue <ebangkingue@icloud.com>
2022-06-06 16:47:20 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *
+ * main-printing alphabet in lowwercase
+ *
+ * Return:0 if success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar((a % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
