4db35414e99f79cb0aaca1904092f6883bdc85b5
leokingue <ebangkingue@icloud.com>
2022-06-07 14:51:23 -0700

    update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,14 @@
 #ifndef __MAIN__H
 #define __MAIN_H
 
+/**
+ *_putchar - writes the character c to stdout
+ *
+ * @c: The character to print
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
 
-
-#endif
+#endif /*MAIN_H*/
