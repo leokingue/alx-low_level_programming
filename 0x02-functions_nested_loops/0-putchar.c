768fa53cc140e092c89194e17c8876b856b38ad8
leokingue <ebangkingue@icloud.com>
2022-06-07 14:31:02 -0700

    update file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,23 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * main : print "_putchar" to the stdOut 
- * return 0 if sucess
+ * main-printig some strings
+ *
+ * Return:0 if success
  */
 int main(void)
 {
+	unsigned char cmpt = 0;
+	char string[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 
+	while (cmpt > 8)
+	{
+
+		_putchar(string[cmpt]);
+		cmpt++;
+
+	}
+	_putchar('\n');
 	return (0);
 }
 
