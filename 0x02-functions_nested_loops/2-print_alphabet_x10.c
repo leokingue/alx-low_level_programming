95ba7cb1d3bcea2b661fa9f5a3d1931c395641a9
leokingue <ebangkingue@icloud.com>
2022-06-07 17:28:42 -0700

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet using your _putchar function
+ *
+ * Return: 0 for success
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+}
