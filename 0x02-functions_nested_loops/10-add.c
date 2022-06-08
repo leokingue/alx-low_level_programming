6b67f195c2dcb678a6dd168b1421f7c6f41c55c3
leokingue <ebangkingue@icloud.com>
2022-06-08 15:23:49 -0700

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add-adding two numbers
+ *
+ * @a:first parameter
+ * @b:second parameter
+ * Return:value
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
