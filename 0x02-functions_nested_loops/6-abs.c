eb3863d8ae7b777278bb058525b0e64ff3afcb68
leokingue <ebangkingue@icloud.com>
2022-06-08 15:04:01 -0700

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include"main.h"
+/**
+ *_abs- printing absolute value of any number
+ *
+ * @n:parameter for the function
+ * Return:0 if succcess
+ */
+int _abs(int n)
+{
+	int m;
+
+	m = abs(n);
+	return (n);
+}
