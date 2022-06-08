a1aad08fbe195840cb4a97b413028cc75a8265e2
leokingue <ebangkingue@icloud.com>
2022-06-08 14:39:17 -0700

    update

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isalpha-checks for alphabetic character.
+ *@c: int to check
+ *Return: int
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
