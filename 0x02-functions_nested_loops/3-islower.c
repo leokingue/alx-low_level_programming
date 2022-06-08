197eba1ab67558d96a189e34518fbcca4eda3d8f
leokingue <ebangkingue@icloud.com>
2022-06-08 14:16:08 -0700

    updat

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,12 @@
  *_islower- checks if char us lower case
  *@c: integer c to check
  * Return: int
- * _verifyOtherLetter- checks if others char us lower case
- * @c: int c to check
- * Return: int
+ *
  */
 int _verifyOtherLetter(int c);
 int _islower(int c)
 {
-	if (c >= 97 || c <= 122 && _verifyOtherLetter(c))
+	if ((c >= 97 || c <= 122) && (_verifyOtherLetter(c)))
 	{
 		return (1);
 	}
@@ -19,6 +17,11 @@ int _islower(int c)
 		return (0);
 	}
 }
+/**
+ *_verifyOtherLetter- checks if other char us lower case
+ *@c: in c to check
+ *Return: int
+ */
 int _verifyOtherLetter(int c)
 {
 	if (c == 0 || c == 20 || c == 70 || c == 'U' || c == 'I' || c == 'H')
