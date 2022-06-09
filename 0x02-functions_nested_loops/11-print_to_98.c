0c0bcc6dfa08fc4e3894ae6395e1d727069fe4bb
leokingue <ebangkingue@icloud.com>
2022-06-08 17:09:48 -0700

    message

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,40 +8,16 @@
  */
 void print_to_98(int n)
 {
-	int i;
-	int j;
-
-	if (n < 98)
-	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i != 98)
-			{
-				printf("%d,", i);
-				printf(" ");
-			}
-			else
-			{
-				printf("%d", i);
-			}
-		}
-		printf("\n");
-	}
-
 	if (n >= 98)
 	{
-		for (j = n; j >= 98; j--)
-		{
-			if (j != 98)
-			{
-				printf("%d,", j);
-				printf(" ");
-			}
-			else
-			{
-				printf("%d", j);
-			}
-		}
-		printf("\n");
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
