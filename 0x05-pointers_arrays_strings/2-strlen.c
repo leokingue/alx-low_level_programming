240012a558931c90eff6e80a8e5a4067bb944cad
leokingue <ebangkingue@icloud.com>
2022-06-14 13:15:15 -0700

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - a function that returns
+ * the length of a string.
+ * @s: char pointer argument
+ * Return: a number
+ */
+int _strlen(char *s)
+{
+	int value = sizeof(s) / sizeof(char);
+
+	return (value);
+}
