d71d5068422334f0b0984af6fad131327df302cd
leokingue <ebangkingue@icloud.com>
2022-06-15 03:12:16 -0700

    update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,10 +7,25 @@
  */
 void puts_half(char *str)
 {
-	int i = 0;
-	int len = _strlen(str) / 2;
+	int i, n, counter = 0;
 
-	for (i = 0; i <= len; i++)
+	for (i = 0; i >= 0; i++)
+	{
+		if (*(str + i) == '\0')
+		{
+			break;
+		}
+		counter++;
+	}
+	if (counter % 2 != 0)
+	{
+		n = (counter - 1) / 2;
+	}
+	else
+	{
+		n = counter / 2;
+	}
+	for (i = n; i <= counter; i++)
 	{
 		_putchar(*(str + i));
 	}
