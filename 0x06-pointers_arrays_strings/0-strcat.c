6e6af71f114c9d7e8419f5fb3576dc5f40b9a3e5
leokingue <ebangkingue@icloud.com>
2022-06-16 04:01:31 -0700

    update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,17 +7,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, a = 0
-	static char DESTINATION[];
+	int i;
+	int a;
 
 	for (i = 0; dest[i] != '\0'; i++)
-	{
-		DESTINATION[i] = dest[i];
-	}
+		;
 	for (a = 0; src[a] != '\0'; a++)
 	{
-		DESTINATION[i] = src[a];
+		dest[i] = src[a];
 		i++;
 	}
-	return (DESTINATION);
+	return (dest);
 }
