6bc3714e10b0170d77af978fb15f90ec3c840be8
leokingue <ebangkingue@icloud.com>
2022-06-20 13:16:43 -0700

    update my memset file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 /**
  * _memset - a function that fills memory with a constant byte.
  * @s: s pointer
- * @b: constant octal b
+ * @b: character to print on s
  * @n: bytes of memory
  * Return: s
  */
@@ -10,8 +10,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	for (i = 0; i <= sizeof(char) * n; i++)
+	for (; i < sizeof(char) * n; i++)
 	{
+		if (*(s + i) == '\0')
+			break;
 		*(s + i) = b;
 	}
 	return (s);
---
+++ 0x07-pointers_arrays_strings/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - main function test
+ * Return: 0
+ */
+int main(void)
+{
+	char buffer[10];
+
+	_memset(buffer, 'b', sizeof(char) * 10);
+	return (0);
+}
