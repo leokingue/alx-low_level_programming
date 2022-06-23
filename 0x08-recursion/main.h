6e127790090033797073c3d23b74d8539071361d
leokingue <ebangkingue@icloud.com>
2022-06-23 14:48:38 -0700

    add all the Headers Files and create a README file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN__H__
+#define MAIN__H__
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_prime_number(int n);
+
+#endif /*main.h*/
