da595176433fc1a0543583c4b6ce8b9d0fcd5d10
leokingue <ebangkingue@icloud.com>
2022-06-27 10:52:28 -0700

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * whatsmyname - a program that prints its name, followed by a new line.
+ * Return: string value
+ */
+char *whatsmyname(int argc, char *argv[])
+{
+	if (argc < 1)
+		return
+
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN__H__
+#define MAIN__H__
+
+int _putchar(char c);
+char *whatsmyname(void);
+
+
+#endif/*main.h*/
