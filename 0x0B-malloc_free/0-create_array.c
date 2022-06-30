e9e27e36f3207204a73b6db73a5de83462298d5f
leokingue <ebangkingue@icloud.com>
2022-06-29 17:08:48 -0700

    update

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * create_array - creating array of a string and initialize it
+ * @size:size of array
+ * @c:single chairacter to the areray
+ * Return:value
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *array;
+
+	if (size == 0)
+		return (NULL);
+	array = malloc(size * sizeof(c));
+
+	if (array == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		array[i] = c;
+	return (array);
+}
