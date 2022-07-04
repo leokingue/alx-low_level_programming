7ddeac71a39f99af46e245405259867614a61121
leokingue <ebangkingue@icloud.com>
2022-07-04 13:30:51 -0700

    update

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - a program that prints the name of the file
+ * @argc: int argument
+ * @argv: pointer argument
+ * Return: value
+ */
+int main(int argc, char *argv)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
