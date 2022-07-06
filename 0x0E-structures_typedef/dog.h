9b409190338315afe7bfdf77e1cf4fa13c0252e9
leokingue <ebangkingue@icloud.com>
2022-07-05 19:01:12 -0700

    eupdate

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG__H__
+#define DOG__H__
+/**
+ * struct dog - Define a new type struct dog
+ * @name: first member od dog
+ * @age: age of dog
+ * @owner: define as own
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
