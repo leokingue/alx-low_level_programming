317d53ab0736126d2cdc6923325524c80539727e
leokingue <ebangkingue@icloud.com>
2022-07-27 12:09:49 -0700

    commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * list_len - a function that returns the number
+ * of elements in a linked list_t list.
+ * @h: List item
+ * Return: Length of list item
+ */
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+
+	while (h != NULL)
+	{
+		len++;
+		h->next;
+	}
+	return (len);
+}
