cb0dce384add350b8aeef77e0910188975daea68
leokingue <ebangkingue@icloud.com>
2022-09-05 12:47:52 -0700

    update

--- 0x18-dynamic_libraries/_isalpha.c
+++ 0x18-dynamic_libraries/_isalpha.c
@@ -4,7 +4,8 @@
  * @c: the character c
  * Return: 0 or 1
  */
-int _isalpha(char c)
+int _isalpha(int c)
 {
+
 
 }
--- 0x18-dynamic_libraries/_islower.c
+++ 0x18-dynamic_libraries/_islower.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * _islower - check if a char is Lower
  * @c: a char
