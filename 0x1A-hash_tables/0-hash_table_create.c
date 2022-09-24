a2d7daccdda518a8beeba83a33b8dbdc620e6706
leokingue <ebangkingue@icloud.com>
2022-09-23 21:53:08 -0700

    add a function who create a new hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * hash_table_create -  function that creates a hash table.
+ * @size: The  size of the new hash tables
+ * Return: a pointer of the new hash tables
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash_table = malloc(sizeof(hash_table_t) * size);
+
+	if (new_hash_table == NULL)
+	{
+		return (NULL);
+	}
+	return (new_hash_table);
+}
